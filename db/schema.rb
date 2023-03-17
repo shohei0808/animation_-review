32378a7c82bbdbd9fe9a53395132c002c6c450d9
shohey <vagabond.shohei@gmail.com>
2023-03-18 06:05:34 +0900

    ポートフォリオ制作

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_191008) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_171045) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_191008) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
