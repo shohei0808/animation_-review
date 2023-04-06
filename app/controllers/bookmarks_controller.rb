70f0f4fc871950624e9346c8a33d4fd5b251be27
shohey <vagabond.shohei@gmail.com>
2023-04-07 06:12:16 +0900

    ブックマーク機能の非同期処理化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,10 +2,18 @@ class BookmarksController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 
   def destroy
     @post = current_user.bookmarks.find(params[:id]).post
     current_user.unbookmark(@post)
+    respond_to do |format|
+      format.html { redirect_to request.referer, status: :see_other }
+      format.turbo_stream
+    end
   end
 end
