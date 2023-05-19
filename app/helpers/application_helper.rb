02f4b0bf5792efa9fc012ebfffce712d691caba2
shohey <vagabond.shohei@gmail.com>
2023-05-20 05:15:31 +0900

    ランキング機能の実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def rank_image(rank)
+    case rank
+    when 1
+      "ranking1.png"
+    when 2
+      "ranking2.png"
+    when 3
+      "ranking3.png"
+    end
+  end
 end
